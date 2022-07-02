d87cc18e646dca3b295f2d8bee22c40576e7badd
CarlosDiegoC <carlos_diegoh@hotmail.com>
2022-07-02 13:23:32 -0300

    Project Created

---
+++ SalesMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SalesMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {      
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        //APLICAR AQUI OS DBSETS.
+        
+        public DbSet<SalesMVC.Models.Department> Department { get; set; }
+    }
+}
